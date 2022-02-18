fa8fc6a7ba9f387e3ed8a9a87940ed9691c82493
murasakibarachi <abdellahbelhachmi13915@gmail.com>
2022-02-18 01:54:54 +0100

    ex01 in process

---
+++ cpp_module00/ex01/contact.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abbelhac <abbelhac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/18 01:31:05 by abbelhac          #+#    #+#             */
+/*   Updated: 2022/02/18 01:37:48 by abbelhac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class contact{
+	std::string fname;
+	std::string lname;
+	std::string nickname;
+	std::string phone;
+	std::string dsecret;
+};
