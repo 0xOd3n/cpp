3f5977e190729ac10b8ce47aac06a33c86fba36e
murasakibarachi <abdellahbelhachmi13915@gmail.com>
2022-03-05 21:21:41 +0100

    fix multi parameter

--- cpp_module00/ex00/megaphone.cpp
+++ cpp_module00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: abbelhac <abbelhac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 19:38:38 by abbelhac          #+#    #+#             */
-/*   Updated: 2022/02/16 19:43:51 by abbelhac         ###   ########.fr       */
+/*   Updated: 2022/03/05 21:06:42 by abbelhac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,26 @@
 
 int	main(int ac, char **av)
 {
-	if (ac != 2)
+	if (ac < 2 )
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
+	int j;
 	int i;
 
-	i = 0;
-	while (av[1][i])
+	j = 1;
+	while (av[j])
 	{
-		av[1][i] = toupper(av[1][i]);
-		i++;
+		i = 0;
+		while (av[j][i])
+		{
+			av[j][i] = toupper(av[j][i]);
+			std::cout << av[j][i];
+			i++;
+		}
+		j++;
 	}
-	std::cout << av[1] << std::endl;
+	std::cout << std::endl;
 	return (0);
 }
