4c610f5f14804e4b4e2c5414fe8bc8cceb8a725c
Abdellah Belhachmi <abbelhac@e2r7p12.1337.ma>
2022-04-09 03:08:02 +0000

    start cpp Module 06

---
+++ cpp_module06/ex00/conversion.hpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conversion.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abbelhac <abbelhac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/09 02:43:12 by abbelhac          #+#    #+#             */
+/*   Updated: 2022/04/09 02:58:17 by abbelhac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONVERSION_HPP
+# define CONVERSION_HPP
+
+#include <iostream>
+#include <type_traits>
+
+
+
+// void	char_converter(const std::string& str)
+// {
+// 	int		i;
+
+// 	i = 0;
+// 	if (str.c_str()[0] == '+' || str.c_str()[0] == '-')
+// 		i++;
+// 	if (!isdigit(str.c_str()[i]))
+// 	{
+// 		if (str.length() == 1)
+// 			std::cout << "char: '" << str.c_str()[0] << "'" << std::endl;
+// 		else if (is_scientific(str))
+// 			std::cout << "char: impossible" << std::endl;
+// 		else
+// 			std::cerr << "int: Invalid argument!!" << std::endl;
+// 		return ;
+// 	}
+// 	if (!checker(str, i))
+// 		std::cerr << "char: Invalid argument!!" << std::endl;
+// 	else
+// 	{
+// 		if (stoi(str) >= 32 && stoi(str) <= 127)
+// 			std::cout << "char: '" << static_cast<char>(stoi(str)) << "'" << std::endl;
+// 		else
+// 			std::cout << "char: Non displayable" << std::endl;
+// 	}
+// }
+
+
+// char_conversion(std::string type)
+// {
+    
+// }
+
+# endif
