cc468455adb5badba9fb7d401b996463df910380
murasakibarachi <abdellahbelhachmi13915@gmail.com>
2022-04-13 05:07:05 +0000

    cpp Module 08

--- cpp_module08/ex02/MutantStack.hpp
+++ cpp_module08/ex02/MutantStack.hpp
@@ -11,15 +11,71 @@
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
-#define MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
 
 #include <iostream>
-#include <deque>
+#include <string>
 #include <stack>
-template <typename T>
-class MutantStack : public std::stack<T>
+#include <deque>
+#include <vector>
+
+template <typename T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
 {
-    
+	public:
+		typedef typename std::stack<T, Container>::container_type::iterator	iterator;
+		typedef typename std::stack<T, Container>::container_type::const_iterator	const_iterator;
+		typedef typename std::stack<T, Container>::container_type::reverse_iterator	reverse_iterator;
+		typedef typename std::stack<T, Container>::container_type::const_reverse_iterator	const_reverse_iterator;
+		
+		MutantStack() : std::stack<T, Container>()
+        {}
+		MutantStack(const MutantStack& cpy) : std::stack<T, Container>()
+		{
+			*this = cpy;
+		}
+		MutantStack& operator=(const MutantStack& cpy)
+		{
+			if (this != &cpy)
+				std::stack<T, Container>::operator=(cpy);
+			return *this;
+		}
+		~MutantStack(){}
+		iterator	begin()
+		{
+			return (this->c.begin());
+		}
+		iterator	end()
+		{
+			return (this->c.end());
+		}
+		
+		const_iterator	cbegin()
+		{
+			return (this->c.cbegin());
+		}
+		const_iterator	cend()
+		{
+			return (this->c.cend());
+		}
+		
+		reverse_iterator	rbegin()
+		{
+			return (this->c.rbegin());
+		}
+		reverse_iterator	rend()
+		{
+			return (this->c.rend());
+		}
+		
+		const_reverse_iterator	crbegin()
+		{
+			return (this->c.crbegin());
+		}
+		const_reverse_iterator	crend()
+		{
+			return (this->c.crend());
+		}
 };
 
 #endif
